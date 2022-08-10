f5804bd7805aa951dd140522fc76132ce253ac7b
TeaPandaa <CaptPandaa@gmail.com>
2022-08-10 20:14:04 +0200

    Reworked

--- Held og lotto/Program.cs
+++ Held og lotto/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading;
-
-namespace Held_og_lotto
+﻿namespace Held_og_lotto
 {
     internal class Program
     {
@@ -9,40 +6,36 @@ namespace Held_og_lotto
         {
             //Generator of random numbers
             Random rng = new Random();
-
-
-            //A string to store the text later on
-            string text = "";
-            
-            //a for loop that runs 7 times, with the last number being red.
-            for (int i = 0; i < 7; i++)
+            //2 arrays, 1 for the pool of numbers to pick from, and a array for the numbers that is picked
+            int[] avaliableNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
+            int[] drawnNumbers = new int[7];
+            //a count and draw int to use in the loop below
+            int count = 0;
+            int draw = 0;
+            do
             {
-                wait(2);
-                int roll = rng.Next(1, 37);
-                if (i < 6)
+                //picsk a random number
+                draw = rng.Next(0, 35);
+                //checks if the number is above 0, as to see if the value already was used
+                if (avaliableNumbers[draw] > 0)
                 {
-                    Console.Write($"{roll}  ", Task.Delay(20000));
+                    //putting the drawn number down in a array
+                    drawnNumbers[count] = avaliableNumbers[draw];
+                    //removes the draw number from the array of numbers to pick from
+                    avaliableNumbers[draw] = -1;
+                    //a count to put the number in the right order in the array
+                    count++;
                 }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write($"{roll}  ", Task.Delay(2000));
-                }
-            }
 
-            //makeshift timer
-            void wait(double x)
+            } while (drawnNumbers[drawnNumbers.Length - 1] == 0);
+            //a foreach loop to write each number from the drawnNumbers array
+            foreach (int number in drawnNumbers)
             {
-                DateTime t = DateTime.Now;
-                DateTime tf = DateTime.Now.AddSeconds(x);
-
-                while (t < tf)
-                {
-                    t = DateTime.Now;
-                }
+                Console.Write($"{number} ");
+                Thread.Sleep(2000);
             }
-
-
+            //Joker number, couldm't remember if the joker could be the same number as one of the other draw numbers but in this program it can ;)
+            Console.Write($"{rng.Next(0, 35)}", Console.ForegroundColor = ConsoleColor.Red);
             //TO NOT close console yet
             Console.Read();
         }
